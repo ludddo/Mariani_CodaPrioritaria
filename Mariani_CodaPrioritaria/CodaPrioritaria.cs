e0c4ceea4bf7c54e393fcb9c3e9d35cb89e8b155
ludo <mariani.ludovico.studente@itispaleocapa.it>
2024-02-15 10:50:46 +0100

    CompareTo() e Aggiunta()

---
+++ Mariani_CodaPrioritaria/CodaPrioritaria.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mariani_CodaPrioritaria
+{
+    internal class CodaPrioritaria
+    {
+        ArrayList lista = new ArrayList();
+
+        public void Aggiunta(int id)
+        {
+            if (lista.Count == 0)
+            {
+                lista.Add(id);
+            }
+            else 
+            {
+                lista.Insert(CompareTo(lista, id), id);
+            }
+        }
+
+        public int CompareTo(ArrayList ciao, int b)
+        {
+            int indirizzo = 0;
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (int.Parse(ciao[i].ToString()) > b)
+                {
+                    indirizzo = i + 1;
+                }
+                else if (int.Parse(ciao[i].ToString()) < b)
+                {
+                    indirizzo = i;
+                    break;
+                }
+            }
+
+            return indirizzo;
+           
+        }
+
+        public void Visualizza()
+        {
+            int i = 0;
+            foreach (int id in lista) 
+            {
+                Console.WriteLine(lista[i]);
+
+                i++;
+            }
+        }
+    }
+}
--- Mariani_CodaPrioritaria/Program.cs
+++ Mariani_CodaPrioritaria/Program.cs
@@ -10,7 +10,16 @@ namespace Mariani_CodaPrioritaria
     {
         static void Main(string[] args)
         {
+            CodaPrioritaria ciao = new CodaPrioritaria();
 
+            ciao.Aggiunta(1);
+            ciao.Aggiunta(5);
+            ciao.Aggiunta(2);
+            ciao.Aggiunta(6);
+            ciao.Aggiunta(0);
+            ciao.Aggiunta(18);
+
+            ciao.Visualizza();
         }
     }
 }
